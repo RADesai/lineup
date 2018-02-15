32e48ccfb01bfed1357bcadc8abf23a3fc3bf1d6
Raj Ashish Desai <rajdesai94@gmail.com>
2018-02-15 00:43:20 -0500

    Added env vars for mlab db connection in PROD

--- server/api/routes.js
+++ server/api/routes.js
@@ -2,15 +2,17 @@ const path = require('path');
 const controller = require('./controllers/controller.js');
 
 const mongoose = require('mongoose');
-const db = require('./mlab_credentials.js')
+const db_URI = process.env.NODE_ENV === 'PRODUCTION'
+    ? `mongodb://${process.env.USER}:${process.env.PASS}@ds051543.mlab.com:51543/heroku_t3t29z55`
+    : require('./mlab_credentials.js')
 
-mongoose.connect(db, {
+mongoose.connect(db_URI, {
     useMongoClient: true
 }, (error) => {
     if (error) {
-        console.log(`Failed to connect to ${db}`);
+        console.log(`Failed to connect to ${db_URI}`);
     } else {
-        console.log(`Connected to ${db}`);
+        console.log(`Connected to ${db_URI}`);
     }
 });
 
